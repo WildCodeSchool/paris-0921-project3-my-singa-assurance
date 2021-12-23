b2615327bb89b4b8598e5fbd4571dc79c5359a06
Matthieu Lemarchand <lemarchand.matth@gmail.com>
2021-12-23 20:25:49 +0100

    work in progress CSS

--- src/components/HomepagePresentation.jsx
+++ src/components/HomepagePresentation.jsx
@@ -7,7 +7,7 @@ function HomepagePresentation() {
   return (
     <div className={style.mainContainer}>
       <div className={style.text}>
-        <h1>Singa, l&apos;assurance santé pour vos proches en RDC</h1>
+        <h1 className={style.titre}>Singa, l&apos;assurance santé pour vos proches en RDC</h1>
         <div>
           <p> Nous croyons que l&apos;Afrique mérite d&apos;avoir accès à des soins de qualité.</p>
           <p> Singa permet à la diaspora d&apos;offrir à leurs proches une mutuelle santé simple, solidaire et utile</p>
