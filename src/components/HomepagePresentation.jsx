5d406dfe9612020cb959cef3039dc8b10d04c0e9
Matthieu Lemarchand <lemarchand.matth@gmail.com>
2021-12-24 14:18:44 +0100

    Light amendment, version for Vinz starter

--- src/components/HomepagePresentation.jsx
+++ src/components/HomepagePresentation.jsx
@@ -8,11 +8,11 @@ function HomepagePresentation() {
     <div className={style.mainContainer}>
       <div className={style.text}>
         <h1 className={style.titre}>Singa, l&apos;assurance santé pour vos proches en RDC</h1>
-        <div>
-          <p> Nous croyons que l&apos;Afrique mérite d&apos;avoir accès à des soins de qualité.</p>
-          <p> Singa permet à la diaspora d&apos;offrir à leurs proches une mutuelle santé simple, solidaire et utile</p>
-          <p> Et tout ça en soutenant des causes qui ont un impact positif sur l&apos;économie locale</p>
-        </div>
+
+        <p> Nous croyons que l&apos;Afrique mérite d&apos;avoir accès à des soins de qualité.</p>
+        <p> Singa permet à la diaspora d&apos;offrir à leurs proches une mutuelle santé simple, solidaire et utile</p>
+        <p> Et tout ça en soutenant des causes qui ont un impact positif sur l&apos;économie locale</p>
+
         <button>Consulter les offres</button>
       </div>
       <img alt="HomepagePresentationImg" src={HomepagePresentationImg} className={style.mainImg}></img>
