b44253739bbb0c39c71952c9d5d3a380e550e7a5
Matthieu Lemarchand <lemarchand.matth@gmail.com>
2022-01-03 12:29:20 +0100

    CSS implemented for homepageTarif : cards border & hoover

--- src/components/TarifCard.jsx
+++ src/components/TarifCard.jsx
@@ -4,7 +4,9 @@ import style from './style/TarifCard.module.scss';
 const TarifCard = ({ title, couverture, prix, desc }) => {
   return (
     <div className={style.tarifCardContainer}>
-      <h1 className="CardTitle">{title}</h1>
+      <div className={style.titleContainer}>
+        <h1 className="CardTitle">{title}</h1>
+      </div>
       <div>couverture à {couverture}%</div>
       <div> à partir de </div>
       <div>{prix}€</div>
