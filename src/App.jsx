fd8429d606c9377cb61c370e27812c296beb5583
Benjamin <b.girardpro@gmail.com>
2022-01-03 16:11:02 +0100

    Create Route for login page and create account page

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import { Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Homepage from './components/Homepage';
+import LoginPage from './components/LoginPage';
 
 import './App.css';
+import FormSignUp from './components/FormSignUp';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
       <Header />
       <Routes>
         <Route exact path="/" element={<Homepage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/createaccount" element={<FormSignUp />} />
       </Routes>
     </>
   );
