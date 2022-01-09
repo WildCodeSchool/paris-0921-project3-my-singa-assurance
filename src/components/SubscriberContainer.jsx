71aa7c7801ee445f23a3607a13b334044f8671ee
Matthieu Lemarchand <lemarchand.matth@gmail.com>
2022-01-09 16:23:07 +0100

    Fixed CSS for main container in SubscriberPage

--- src/components/SubscriberContainer.jsx
+++ src/components/SubscriberContainer.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 import SubscriberLeftNavBar from './SubscriberLeftNavBar';
 import SubscriberWelcomePage from './SubscriberWelcomePage';
 
+import style from './style/SubscriberContainer.module.scss';
+
 function SubscriberContainer() {
   return (
-    <div>
+    <div className={style.subscriberDownContainer}>
       <SubscriberLeftNavBar />
       <SubscriberWelcomePage />
     </div>
--- src/components/SubscriberPage.jsx
+++ src/components/SubscriberPage.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import SubscriberHeader from './SubscriberHeader';
 import SubscriberContainer from './SubscriberContainer';
 
+import style from './style/Subscriberpage.module.scss';
+
 function SubscriberPage() {
   return (
-    <>
+    <div className={style.subscriberMainContainer}>
       <SubscriberHeader />
       <SubscriberContainer />
-    </>
+    </div>
   );
 }
 
