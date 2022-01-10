057452974032ae0247d4ec6b017e173427d7cf22
Matthieu Lemarchand <lemarchand.matth@gmail.com>
2022-01-10 20:31:51 +0100

    Work in progress : fixing hover css for ConceptButtons

--- src/components/ConceptButtons.jsx
+++ src/components/ConceptButtons.jsx
@@ -3,13 +3,13 @@ import style from './style/ConceptButtons.module.scss';
 
 const ConceptButtons = ({ icone, title, arg }) => {
   return (
-    <div className={style.allConceptButton}>
-      <div className={style.apparentButton}>
+    <ul className={style.allConceptButton}>
+      <li className={style.apparentButton}>
         <img src={icone} alt="icone" />
         <div>{title}</div>
-      </div>
+      </li>
       <ul className={style.ulConceptBtn}>{arg && arg.map((element) => <li key={element}>{element}</li>)}</ul>
-    </div>
+    </ul>
   );
 };
 
