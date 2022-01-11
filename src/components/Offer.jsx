a9baca1b26e1fc9c4c7eee437a3edb2276741faa
Matthieu Lemarchand <lemarchand.matth@gmail.com>
2022-01-11 23:19:17 +0100

    Work in Progress Counter CSS

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -1,17 +1,29 @@
 import React, { useState } from 'react';
 
+import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+
 import style from './style/Offer.module.scss';
 
 function Offer() {
   const [juniorCount, setJuniorCount] = useState(0);
 
+  localStorage.setItem('JuniorCount', juniorCount);
+
   return (
     <div className={style.mainOfferContainer}>
       <h1>Combien de proches souhaitez-vous assurer ?</h1>
-      <div>
-        <div>{juniorCount < 0 ? setJuniorCount(0) : juniorCount}</div>
-        <div onClick={() => setJuniorCount(juniorCount + 1)}>+</div>
-        <div onClick={() => setJuniorCount(juniorCount - 1)}>-</div>
+      <div className={style.countersMainContainer}>
+        <div className={style.ageCounterMainContainer}>
+          <div className={style.ageCounter}>
+            <div>{juniorCount < 0 ? setJuniorCount(0) : juniorCount}</div>
+            <div className={style.arrowBtn}>
+              <ArrowDropUpIcon onClick={() => setJuniorCount(juniorCount + 1)} />
+              <ArrowDropDownIcon onClick={() => setJuniorCount(juniorCount - 1)} />
+            </div>
+          </div>
+          <div className={style.ageCounterTitle}>Juniors</div>
+        </div>
       </div>
     </div>
   );
