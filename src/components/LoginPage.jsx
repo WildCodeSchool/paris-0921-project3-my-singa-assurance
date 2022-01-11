0ce51d3e161dee79727a71a17ab40d60c138b2ae
sophielepert <sophie.lepert@gmail.com>
2022-01-11 10:42:30 +0100

    create the login page connection

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,64 +1,95 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+import axios from 'axios';
 
-import Header from './Header';
 import style from './style/LoginPage.module.scss';
 import Background from '../assets/LoginBackground.png';
 
 function LoginPage() {
-  const { register, handleSubmit } = useForm();
   const navigate = useNavigate();
 
   const handleCreateAccount = () => {
     navigate('/createaccount');
   };
 
-  const onSubmit = (data) => {
-    // eslint-disable-next-line no-console
-    console.log(data);
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().required('Email requis'),
+    password: Yup.string()
+      .required('Mot de passe requis')
+      .matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: 'onTouched',
+    resolver: yupResolver(validationSchema),
+  });
+
+  const onSubmit = async (data) => {
+    localStorage.removeItem('dataConnection');
+    const token = await axios.post('http://localhost:8080/auth/login', data);
+    localStorage.setItem('dataConnection', token.data);
+    // eslint-disable-next-line no-unused-vars
+    const result = await axios.get('http://localhost:8080/subscribers', {
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: 'Bearer ' + localStorage.getItem('dataConnection'),
+      },
+    });
+    navigate('/subscribers');
   };
 
   return (
-    <>
-      <Header />
-      <div className={style.mainLoginPageContainer}>
-        <div className={style.mainText}>
-          <h2 className={style.logintitle}>La distance n&apos;est plus un obstacle</h2>
-          <h2 className={style.logintitle}> à la santé de vos proches</h2>
-          <p className={style.loginText1}>Bon retour parmi nous ! Connectez vous pour accèder </p>
-          <p className={style.loginText2}>à votre espace personnel</p>
-          <form className={style.loginForm} onSubmit={handleSubmit(onSubmit)}>
-            <div className={style.loginEmail}>
-              <label htmlFor="email" className={style.loginEmailLabel}>
-                Adresse mail
-              </label>
-              <input type="text" id="email" className={style.loginEmailInput} placeholder="a.bukasa@gmail.com" name="email" {...register('email')} />
-            </div>
-            <div className={style.loginPassword}>
-              <label htmlFor="password" className={style.loginPasswordLabel}>
-                Mot de passe
-              </label>
-              <input type="password" id="password" className={style.loginPasswordInput} name="password" {...register('password')} />
-            </div>
-            <div className={style.loginOptionButton}>
-              <input type="checkbox" id="rememberMe" />
-              <label htmlFor="checkbox" className={style.loginRememberMe}>
-                Se rappeler de moi
-              </label>
-              <button className={style.loginPasswordForgetButton}>Mot de passe oublié ?</button>
-            </div>
-            <div className={style.loginAccountButton}>
-              <button className={style.loginConnexionButton}>Connexion</button>
-              <button className={style.loginRegisterButton} onClick={handleCreateAccount}>
-                S&apos;inscrire
-              </button>
-            </div>
-          </form>
-        </div>
-        <img src={Background} className={style.backgroundImageLogin} alt="person entering the service" />
+    <div className={style.mainLoginPageContainer}>
+      <div className={style.mainText}>
+        <h2 className={style.logintitle}>La distance n&apos;est plus un obstacle</h2>
+        <h2 className={style.logintitle}> à la santé de vos proches</h2>
+        <p className={style.loginText1}>Bon retour parmi nous ! Connectez vous pour accèder </p>
+        <p className={style.loginText2}>à votre espace personnel</p>
+        <form className={style.loginForm} onSubmit={handleSubmit(onSubmit)}>
+          <div className={style.loginEmail}>
+            <label htmlFor="email" className={style.loginEmailLabel}>
+              Adresse mail
+            </label>
+            <input type="text" id="email" name="email" className={style.loginEmailInput} {...register('email')} placeholder={errors.email?.message} />
+          </div>
+          <div className={style.loginPassword}>
+            <label htmlFor="password" className={style.loginPasswordLabel}>
+              Mot de passe
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className={style.loginPasswordInput}
+              {...register('password')}
+              placeholder={errors.password?.message}
+            />
+          </div>
+          <div className={style.loginOptionButton}>
+            <input type="checkbox" id="rememberMe" />
+            <label htmlFor="checkbox" className={style.loginRememberMe}>
+              Se rappeler de moi
+            </label>
+            <button className={style.loginPasswordForgetButton}>Mot de passe oublié ?</button>
+          </div>
+          <div className={style.loginAccountButton}>
+            <input className={style.loginConnexionButton} type="submit" />
+            <button className={style.loginRegisterButton} onClick={handleCreateAccount}>
+              S&apos;inscrire
+            </button>
+          </div>
+        </form>
       </div>
-    </>
+      <img src={Background} className={style.backgroundImageLogin} alt="person entering the service" />
+    </div>
   );
 }
 
