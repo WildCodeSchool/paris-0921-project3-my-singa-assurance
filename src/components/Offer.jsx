5dbb0cb94c183dfb4832537cb6f17bbee626e1a0
Matthieu Lemarchand <lemarchand.matth@gmail.com>
2022-01-11 13:50:57 +0100

    Work in Progress Offer page

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 import style from './style/Offer.module.scss';
 
 function Offer() {
-  return <div className={style.mainOfferContainer}>OFFER</div>;
+  return (
+    <div className={style.mainOfferContainer}>
+      <h1>Combien de proches souhaitez-vous assurer ?</h1>
+    </div>
+  );
 }
 
 export default Offer;
