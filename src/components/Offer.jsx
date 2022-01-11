d9e60bec2b0ca96201d21afaf754617814457722
Matthieu Lemarchand <lemarchand.matth@gmail.com>
2022-01-11 21:29:27 +0100

    set jsx for juniorCount

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import style from './style/Offer.module.scss';
 
 function Offer() {
+  const [juniorCount, setJuniorCount] = useState(0);
+
   return (
     <div className={style.mainOfferContainer}>
       <h1>Combien de proches souhaitez-vous assurer ?</h1>
+      <div>
+        <div>{juniorCount < 0 ? setJuniorCount(0) : juniorCount}</div>
+        <div onClick={() => setJuniorCount(juniorCount + 1)}>+</div>
+        <div onClick={() => setJuniorCount(juniorCount - 1)}>-</div>
+      </div>
     </div>
   );
 }
