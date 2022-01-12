d4b3b2026f86e17324483177eb61eeace15e4dd7
Benjamin <b.girardpro@gmail.com>
2022-01-12 14:35:52 +0100

    Remove useContext cannot be use out of react scope

--- src/services/axios.service.js
+++ src/services/axios.service.js
@@ -1,7 +1,5 @@
-import { useContext } from 'react';
 import axios from 'axios';
 import jwt from 'jwt-decode';
-import SubscriberInfoContext from '../context/SubscriberInfoContext';
 
 const options = {
   'Content-Type': 'application/json',
@@ -10,38 +8,29 @@ const options = {
 
 const URL = 'http://localhost:8080';
 
-const { setDecodedToken, setSubscriberInfo, setRecipientsInfo } = useContext(SubscriberInfoContext);
-
-const createSubscriberAccount = async (data) => {
+export const createSubscriberAccount = async (data) => {
   const subscriberCreated = await axios.post(`${URL}`, data);
   return subscriberCreated;
 };
 
-const logIn = async (data) => {
+export const logIn = async (data) => {
   const token = await axios.post(`${URL}/auth/login`, data);
   localStorage.setItem('x-access-token', token.data);
-  setDecodedToken(jwt(token));
+  const decoded = jwt(localStorage.getItem('x-access-token'));
+  return decoded.data;
 };
 
-const getSubscriberInfo = async (id) => {
+export const getSubscriberInfo = async (id) => {
   const [subscriber] = await axios.get(`${URL}/subscriber/${id}`, options);
-  setSubscriberInfo(subscriber);
+  return subscriber;
 };
 
-const createRecipient = async (data) => {
+export const createRecipient = async (data) => {
   const recipient = await axios.post(`${URL}/recipients`, data, options);
   return recipient;
 };
 
-const getRecipients = async (id) => {
+export const getRecipients = async (id) => {
   const [recipients] = await axios.get(`${URL}/recipients/${id}`, options);
-  setRecipientsInfo(recipients);
-};
-
-module.exports = {
-  logIn,
-  createSubscriberAccount,
-  getSubscriberInfo,
-  createRecipient,
-  getRecipients,
+  return recipients;
 };
