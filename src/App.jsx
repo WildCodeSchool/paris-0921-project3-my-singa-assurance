e843e17c41a437cff09595546911d80bf62e4fb3
Benjamin <b.girardpro@gmail.com>
2022-01-13 18:18:32 +0100

    Add two nested route of form components

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,10 @@ import Homepage from './components/Homepage';
 import LoginPage from './components/LoginPage';
 import ActusPage from './components/ActusPage';
 import SubscriberPage from './components/SubscriberPage';
-import FormSignUp from './components/FormSignUp';
 import SubscriberWelcomePage from './components/SubscriberWelcomePage';
-
+import FormPage from './pages/FormPage';
+import FormSignUpStep1 from './formSignUp/FormSignUpStep1';
+import FormSignUpStep2 from './formSignUp/FormSignUpStep2';
 
 import './App.css';
 
@@ -18,7 +19,10 @@ function App() {
         <Route exact path="/" element={<Homepage />} />
         <Route path="/actus" element={<ActusPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/createaccount" element={<FormSignUp />} />
+        <Route path="/createaccount" element={<FormPage />}>
+          <Route path="step1" element={<FormSignUpStep1 />} />
+          <Route path="step2" element={<FormSignUpStep2 />} />
+        </Route>
         <Route path="/subscribers" element={<SubscriberPage />} />
         <Route path="/welcome" element={<SubscriberWelcomePage />} />
       </Routes>
