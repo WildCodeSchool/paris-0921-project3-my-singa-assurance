9d114e91b4a632aed1717bdfde0b3a179e6d6958
sophielepert <sophie.lepert@gmail.com>
2022-01-17 09:18:12 +0100

    fixes css footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,41 +9,41 @@ import style from './style/Footer.module.scss';
 function Footer() {
   return (
     <div className={style.mainFooter}>
-      <div className={style.bloc}>
+      <div className={style.footerBlock}>
         <div className={style.logoFooter}>
-          <img className={style.logo} src={Logo} alt="Singa Logo" />
-          <p className={style.singa}>singa </p>
+          <p className={style.footerSinga}>singa </p>
+          <img className={style.footerLogo} src={Logo} alt="Singa Logo" />
         </div>
-        <div>
-          <p> Bonjour@Singa.com</p>
-          <p>75 rue Saint-Martin</p>
-          <p>75008 PARIS</p>
-        </div>
-      </div>
-
-      <div className={style.bloc}>
-        <div className={style.ressources}>Ressources</div>
-        <div className={style.menu}>
-          <p>leBlog</p>
-          <p>FAQ</p>
-          <p>Présence internationale</p>
-          <p>Dictionnnaire assurance</p>
+        <div className={style.footerMenuBlock}>
+          <p className={style.footerMenuText}>Bonjour@Singa.com</p>
+          <p className={style.footerMenuText}>75 rue Saint-Martin</p>
+          <p className={style.footerMenuText}>75008 PARIS</p>
         </div>
       </div>
 
-      <div className={style.bloc}>
-        <div className={style.ressources}>Ressources</div>
-        <div className={style.menu}>
-          <p>On recrute</p>
-          <p>Mentions légales</p>
-          <p>Présence internationale</p>
-          <p>Dictionnnaire assurance</p>
+      <div className={style.footerBlock}>
+        <div className={style.footerRessources}>Ressources</div>
+        <div className={style.footerMenu}>
+          <p className={style.footerMenuText}>Blog</p>
+          <p className={style.footerMenuText}>FAQ</p>
+          <p className={style.footerMenuText}>Présence internationale</p>
+          <p className={style.footerMenuText}>Dictionnnaire assurance</p>
         </div>
       </div>
 
-      <div className={style.infos}>
-        <div className={style.suivezsinga}>Suivez Singa</div>
-        <div className={style.iconContainer}>
+      <div className={style.footerBlock}>
+        <div className={style.footerRessources}>Entreprise</div>
+        <div className={style.footerMenu}>
+          <p className={style.footerMenuText}>On recrute</p>
+          <p className={style.footerMenuText}>Mentions légales</p>
+          <p className={style.footerMenuText}>Présence internationale</p>
+          <p className={style.footerMenuText}>Dictionnnaire assurance</p>
+        </div>
+      </div>
+
+      <div className={style.footerInfos}>
+        <div className={style.footerSuivezSinga}>Suivez Singa</div>
+        <div className={style.footerIconContainer}>
           <FacebookIcon />
           <TwitterIcon />
           <YouTubeIcon />
