85b48b1983825244423e9601ac379fda14fff24f
Matthieu Lemarchand <lemarchand.matth@gmail.com>
2022-01-26 16:18:17 +0100

    work in progress SubscriberOneRecipient CSS

--- src/subscriber/SubscriberOneRecipient.jsx
+++ src/subscriber/SubscriberOneRecipient.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import style from './style/SubscriberOneRecipient.module.scss';
 import Sophie from '../assets/MpandaSophie.png';
 
@@ -17,13 +18,47 @@ function SubscriberOneRecipient() {
     location && (
       <div className={style.subscriberWelcomePageBeneficiariesImageMain}>
         <div className={style.subscriberWelcomePageBeneficiariesImagePortrait}>
+          <ArrowBackIosIcon onClick={goBack} />
           <img src={Sophie} alt="beneficiary portrait" className={style.subscriberWelcomePageBeneficiariesImagePortraitdetail} />
           <div className={style.subscriberWelcomePageBeneficiariesImagePortraitdetailBreak}>
             <p className={style.subscriberWelcomePageBeneficiariesImagePortraitdetailText}>{location.state.recipient.first_name}</p>
-            <p className={style.subscriberWelcomePageBeneficiariesImagePortraitdetailText}>TEST TEST</p>
+            <p className={style.subscriberWelcomePageBeneficiariesImagePortraitdetailText2}>{location.state.recipient.last_name}</p>
           </div>
         </div>
-        <button onClick={goBack}> Back </button>
+        <div className={style.mainCoordinatesContainer}>
+          <div className={style.coordinates}>
+            <p className={style.coordinatesWord}>Coordonnées</p>
+            <p className={style.clientId}>N° client : {location.state.recipient.recipient_id}</p>
+          </div>
+          <div className={style.recipientContainer}>
+            <div className={style.recipient}>
+              <p style={{ fontSize: '13px' }}> Titulaire</p>
+              <div className={style.name}>
+                <p> {location.state.recipient.first_name} </p>
+                <p className={style.lastName}> {location.state.recipient.last_name} </p>
+              </div>
+            </div>
+            <p className={style.modify}>Modifier</p>
+          </div>
+          <div className={style.recipientContainer2}>
+            <div className={style.recipient}>
+              <p style={{ fontSize: '13px' }}> Numéro de téléphone</p>
+              <div className={style.name}>
+                <p className={style.lastName}> {location.state.recipient.phone_number} </p>
+              </div>
+            </div>
+            <p className={style.modify}>Modifier</p>
+          </div>
+          <div className={style.recipientContainer3}>
+            <div className={style.recipient}>
+              <p style={{ fontSize: '13px' }}> Adresse </p>
+              <div className={style.name}>
+                <p className={style.lastName}> {location.state.recipient.address} </p>
+              </div>
+            </div>
+            <p className={style.modify}>Modifier</p>
+          </div>
+        </div>
       </div>
     )
   );
