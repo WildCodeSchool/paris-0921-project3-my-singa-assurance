e6d18574738a07e51e2c29d355ec64ebdfe724cc
sophielepert <sophie.lepert@gmail.com>
2022-02-02 16:48:43 +0100

    undo css fixes of the concept card

--- src/homepage/ConceptButtons.jsx
+++ src/homepage/ConceptButtons.jsx
@@ -3,8 +3,8 @@ import style from './style/ConceptButtons.module.scss';
 
 function ConceptButtons({ icone, title, arg }) {
   return (
-    <div className={style.allConceptButtonTarifCard}>
-      <div className={style.apparentButtonTarifCard}>
+    <div className={style.allConceptButton}>
+      <div className={style.apparentButton}>
         <img src={icone} alt="icone" />
         <div>
           {title} <span style={{ color: '#2AD63E' }}> + </span>
