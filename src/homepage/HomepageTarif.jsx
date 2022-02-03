f83b1e3f8ed8c650bf1e1250f96d016a3a96f31c
sophielepert <sophie.lepert@gmail.com>
2022-02-03 16:57:17 +0100

    css fixes add some Lorem Ipsum to spice it up

--- src/homepage/HomepageTarif.jsx
+++ src/homepage/HomepageTarif.jsx
@@ -36,7 +36,10 @@ function HomepageTarif() {
     <div className={style.mainTarifCard}>
       <h2>Nos Formules</h2>
       <div className={style.tarifResume}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. TES TEST
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+        anim id est laborum.
       </div>
       <div className={style.tarifContainer}>
         {Formules.map((card) => {
